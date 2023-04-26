3931862d1cdc07215de0230ed4e6dbf163662739
Kate & Miki <nathovaekaterina@gmail.com>
2023-04-26 16:28:13 +0300

    со всеми корректировками

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
+"""функция, меняющая все буквы в строке на заглавные"""
 def lower(string):
     return string.lower()
+
+"""функция, делающая заглавными первые 2 буквы"""
+def first_two_lower(string):
+    return (string[1,2].lower())
